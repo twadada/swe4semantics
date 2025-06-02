a39ac3c3c9406c66967f19a395125f5bff72fd9b
Takashi Wada <takashi.wada.764@gmail.com>
2025-06-02 15:40:12 +0900

    Create util.py
    add util

---
+++ util.py
@@ -0,0 +1,56 @@
+import numpy as np
+import string
+
+def load_w2v(file):
+    word2vec = {}
+    with open(file, 'r', errors='ignore') as f:
+        dim = None
+        for line in f:
+            line = line.rstrip('\n')
+            line = line.rstrip(' ').split(' ')
+            if len(line) == 2:
+                print("skip the first line")
+                continue
+            w = line[0]
+            vec = line[1:]
+            if dim is None:
+                dim = len(vec)
+            else:
+                if len(vec) == 0:
+                    print("skip the zero vector")
+                    continue
+                assert len(vec) == dim, str(len(vec)) + "::" + str(dim)
+            word2vec[w] = np.array([float(x) for x in vec])
+    return word2vec, dim
+
+
+def encode_text(text, word_tokenizer, model_tokenizer, punct_list, word2vec, edim):
+    eps =  0.00000001
+    tokens = [x[0] for x in word_tokenizer(text)]
+    sent_emb = []
+    for w in tokens:
+        if w not in punct_list:
+            word_vec = word2vec.get(w)
+            if word_vec is None:
+                word_vec = word2vec.get(w.lower())
+            if word_vec is not None:
+                sent_emb.append(word_vec)
+            elif model_tokenizer is not None:
+                subwords = model_tokenizer.tokenize(w)
+                if len(subwords):
+                    while len(subwords) > 1:
+                        subwords = subwords[:-1]
+                        subwords_str = "".join(subwords).replace("##", "")  # .replace("▁", "")
+                        word_vec = word2vec.get(subwords_str)
+                        if word_vec is None:
+                            word_vec = word2vec.get(subwords_str.lower())
+                        if word_vec is not None:
+                            sent_emb.append(word_vec)
+                            break
+    if len(sent_emb) > 0:
+        sent_emb = np.sum(sent_emb, axis=0)  # n, edim
+        sent_emb = sent_emb / (np.linalg.norm(sent_emb) + eps)
+    else:
+        sent_emb = np.zeros(edim)
+    return sent_emb
+
